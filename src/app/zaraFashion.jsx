1642607879011df06a3c00fcfca0b892ae8392c7
Hiya Kothari <hiyakothari2@gmail.com>
2025-09-02 12:16:53 +0530

    commit changes

--- src/app/zaraFashion.jsx
+++ src/app/zaraFashion.jsx
@@ -5,7 +5,7 @@ import Section2 from "./section2/section2";
 import './section2/section2.css';
 
 import './Section3/section3.css';
-import Section3 from "./Section3/Section3";
+import Section5 from "./Section5/Section5";
 import './Section4/section4.css';
 import './Section5/section5.css';
 export default function ZaraFashion(){
@@ -13,7 +13,7 @@ export default function ZaraFashion(){
         <div className="zara-fashion">
             <Section1 />
             <Section2 />
-            <Section3 />
+            <Section5 />
         </div>
     )
 }
